2dcba464dfb08e0b5f649f7e3a72306f8b86adcc
Rose-Rose-Rose <mironova_rosa@mail.ru>
2019-02-24 20:01:41 +0300

    first variant

---
+++ Sim.java
@@ -0,0 +1,10 @@
+package Sim;
+
+
+public class Sim {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("I'm Rose");
+        System.out.println("I'm 20");
+    }
+}
